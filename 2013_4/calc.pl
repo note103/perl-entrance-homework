9fbcb3dda9bfebf430f58c9dc4b3e768c74fd869
Hiroaki Kadomatsu <note103@gmail.com>
2013-11-27 18:16:05 +0900

    Create calc.pl

---
+++ 2013_4/calc.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Data::Dumper;
+
+#2013/11/27 17:59:21
+#Perl入学式、2013年#4の復習問題1問目
+#https://github.com/perl-entrance-org/workshop-2013-04/blob/master/practice.md
+
+sub calc {
+    my ($num1, $num2) = @_;
+    my $add = $num1 + $num2;
+    my $sub = $num1 - $num2;
+    my $mul = $num1 * $num2;
+    my $div = $num1 / $num2;
+    my $mod = $num1 % $num2;
+    my $results = {
+        add => "$add",
+        sub => "$sub",
+        mul => "$mul",
+        div => "$div",
+        mod => "$mod",
+    };
+    return $results;
+}
+
+my $var = calc(6, 3);
+print Dumper $var;
