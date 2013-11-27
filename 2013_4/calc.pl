294116debe703f2f2d1a2b2f497b732cbc06a4e7
Hiroaki Kadomatsu <note103@gmail.com>
2013-11-28 00:14:22 +0900

    Add calc2.pl & Modify calc.pl

--- 2013_4/calc.pl
+++ 2013_4/calc.pl
@@ -3,6 +3,8 @@ use strict;
 use warnings;
 use Data::Dumper;
 
+#2つの数値を引数として, add, sub, mul, div, mod といった key に演算結果を代入し, そのハッシュリファレンスを返り値とするような関数 calc を作成してください.
+
 sub calc {
     my ($num1, $num2) = @_;
     my $add = $num1 + $num2;
---
+++ 2013_4/calc2.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Data::Dumper;
+
+#calc.plで作成した関数における引数が数字であるかどうか正規表現を使って判定するように改良してください.
+#数値以外が引数であった場合は undef を返すようにしてください.
+
+sub calc {
+    my ($num1, $num2) = @_;
+    if ($num1 =~ /\D/ || $num2 =~ /\D/) {
+        return "undef\n";
+    } else {
+        my $add = $num1 + $num2;
+        my $sub = $num1 - $num2;
+        my $mul = $num1 * $num2;
+        my $div = $num1 / $num2;
+        my $mod = $num1 % $num2;
+        my $results = {
+            add => "$add",
+            sub => "$sub",
+            mul => "$mul",
+            div => "$div",
+            mod => "$mod",
+        };
+        return $results; 
+    }
+}
+
+my $var = calc(14, 3); #両方数字
+my $hoge = calc('str', 3); #前者が文字列
+my $foo = calc(14, 'str'); #後者が文字列
+my $piyo = calc('str1', 'str2'); #両方文字列
+
+print Dumper $var;
+print Dumper $hoge;
+print Dumper $foo;
+print Dumper $piyo;
+
+
+__END__
+出力結果
