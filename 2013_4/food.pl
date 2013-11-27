b79ec683dcf4faa1fa3083b25bded7a3cc9721d4
Hiroaki Kadomatsu <note103@gmail.com>
2013-11-28 08:53:41 +0900

    Add food.pl & Modify readme

---
+++ 2013_4/food.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my $data = q{
+    papix : sushi
+    moznion : soba
+    boolfool : sushi
+    macopy : sushi
+};
+
+#q{};って書き方初めて見た
+
+my @str = split (/\n/, $data);
+
+my $count;
+for my $str (@str) {
+    if ($str =~ / : /) {
+        $count++;
+    }
+}
+
+print "$count\n";
+
