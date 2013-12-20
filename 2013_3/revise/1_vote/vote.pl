f1950ca1e4be410aca54f18bba58cb267105ca57
Hiroaki Kadomatsu <note103@gmail.com>
2013-12-21 01:54:21 +0900

    Create vote.pl

---
+++ 2013_3/revise/1_vote/vote.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my $me = {
+    name => 'hiroaki',
+    food => [qw/egg rice potato beef/],
+};
+my $bob = {
+    name => 'Bob',
+    food => [qw/vegi egg suiton beef/],
+};
+my $alice = {
+    name => 'Alice',
+    food => [qw/vegi egg tomato rice/],
+};
+
+my @people = ($me, $bob, $alice);
+
+my %count_foods;
+
+for my $people (@people) {
+    my $person_foods = $people->{food};
+    for my $name_food (@$person_foods) {
+        $count_foods{$name_food}++;
+    }
+}
+
+my %ranking;
+my $ranking = \%ranking;
+
+for my $name_food2 (keys %count_foods) {
+    my $voted_num = $count_foods{$name_food2};
+    push @{$ranking{$voted_num}}, $name_food2;
+}
+
+for my $sorted_ranking (sort {$b <=> $a} keys %ranking) {
+    print "$sorted_ranking: \n";
+    for my $sorted_foods (@{$ranking->{$sorted_ranking}}) {
+        print "  $sorted_foods\n";
+    }
+}
