942ed2fc0adc9da80be1bbea6f8240076e88973b
Hiroaki Kadomatsu <note103@gmail.com>
2013-12-29 02:07:20 +0900

    Modify is to :

--- 2013_5/practice/YAPC/YAPC.pl
+++ 2013_5/practice/YAPC/YAPC.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use YAPC;
 
-print "Year is ".YAPC::year()."\n";
-print "Month is ".YAPC::month()."\n";
-print "Day is ".YAPC::day()."\n";
-print "True or False(0) is ".YAPC::is_yet('2010/2/27')."\n";
+print "Year: ".YAPC::year()."\n";
+print "Month: ".YAPC::month()."\n";
+print "Day: ".YAPC::day()."\n";
+print "True or False(0): ".YAPC::is_yet('2010/2/27')."\n";
