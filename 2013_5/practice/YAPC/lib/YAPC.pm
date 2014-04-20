2110621aad92abf726e277387a44f6a4c04092cf
Hiroaki Kadomatsu <note103@gmail.com>
2014-04-20 12:18:01 +0900

    Modify YAPC.pm

--- 2013_5/practice/YAPC/lib/YAPC.pm
+++ 2013_5/practice/YAPC/lib/YAPC.pm
@@ -17,22 +17,18 @@ package YAPC {
 
     sub is_yet {
         my $str = shift;
-        if (defined $str) {
-            if ($str =~ /(\d{4})\/(\d{2})\/(\d{2})/) {
-                if ($1 < 2014) {
-                    return 'Yet!';
-                } elsif ($1 == 2014 && $2 < 8) {
-                    return 'Yet!';
-                } elsif ($1 == 2014 && $2 == 8 && $3 <= 28) {
-                    return 'Yet!';
-                } else {
-                    return 0;
-                }
+        if (defined $str && $str =~ /(\d{4})\/(\d{2})\/(\d{2})/) {
+            if ($1 < 2014) {
+                return 'Yet!';
+            } elsif ($1 == 2014 && $2 < 8) {
+                return 'Yet!';
+            } elsif ($1 == 2014 && $2 == 8 && $3 <= 28) {
+                return 'Yet!';
             } else {
                 return 0;
             }
         } else {
-            return undef;
+            return 0;
         }
     }
 }
